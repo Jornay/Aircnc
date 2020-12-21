c4de54c1fd62949ef4c497678b152e78945e0683
Lucas de Abreu Furtado Garcia <52716819+Jornay@users.noreply.github.com>
2020-12-21 15:22:41 -0300

    alteração em relação a Listagem dos spots via Mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -9,19 +9,19 @@ export default function List(){
 
     const [techs, setTechs]  =useState([]);
 
-    //useEffect(() => {
-    //    AsyncStorage.getItem('techs').then(storagedTechs => {
-    //        const techsArray = storagedTechs.split(',').map(tech => tech.trim());
-//
-    //        setTechs(techsArray);
-    //    })
-    //}, []);
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
 
     return (
     <SafeAreaView style = {styles.container}>
         <Image style = {styles.logo} source = {logo}/>
 
-        <SpotList />
+        {techs.map(tech => <SpotList Key={tech} tech= {tech}/>)}
     </SafeAreaView>
     )
 }
