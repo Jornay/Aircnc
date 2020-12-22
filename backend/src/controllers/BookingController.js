50acd03faa2c0bd5246d861262e93d4ecdc827f8
Lucas de Abreu Furtado Garcia <52716819+Jornay@users.noreply.github.com>
2020-12-22 16:19:29 -0300

    novas funcionalidades

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -10,6 +10,8 @@ module.exports = {
         const { date } = req.body;
 
 
+
+
         const booking = await Booking.create({
             user : user_id,
             spot : spot_id,
@@ -17,6 +19,12 @@ module.exports = {
         });
         
         await booking.populate('spot').populate('user').execPopulate();
+
+        const ownerSocket = req.connectedUsers[booking.spot.user]
+
+        if(ownerSocket) {
+            req.io.to(ownerSocket).emit('booking_request' , booking);
+        }
 
         return res.json(booking);
     
--- frontend/src/pages/DashBoard/Index.js
+++ frontend/src/pages/DashBoard/Index.js
@@ -1,14 +1,30 @@
-import React , { useEffect, useState } from 'react';
+import React , { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import socketio from 'socket.io-client'; 
 
 import './styles.css';
 
 export default function DashBoard(){
 
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
 
-    useEffect(( ) => {
+
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketio('http://localhost:3333', {
+        query: { user_id},
+    }) , [ user_id]); 
+
+    useEffect(() => {
+
+        socket.on('booking_request' , data => {
+            setRequests([... requests, data]);
+        })
+
+    }, [requests, socket]); 
+
+    useEffect(() => {
         async function loadSpots(){
             const user_id = localStorage.getItem('user');
             const response = await api.get('/dashboard' , {
@@ -23,6 +39,17 @@ export default function DashBoard(){
     
     return (
         <>
+            <ul className="notifications">
+                {requests.map( request => (
+                    <li key = {request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong> esta solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.spot.date}</strong>
+                        </p>
+                        <button className="accept">ACEITAR</button>
+                        <button className="reject">RECUSAR</button> 
+                    </li>
+                ))}
+            </ul>
             <ul className = "spot-list">
                 {spots.map( spot => (
                     <li key={spot._id}>
