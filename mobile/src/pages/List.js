575c5aeb6ec6c9d7d0f0f90a531d0fe3b1c18927
Lucas de Abreu Furtado Garcia <52716819+Jornay@users.noreply.github.com>
2020-12-24 16:38:04 -0300

    criação de opção de logout

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 //import socketio from 'socket.io-client';
-import {  Alert, Platform,SafeAreaView ,ScrollView,StyleSheet , Image, AsyncStorage} from 'react-native';
+import {  Alert,TouchableOpacity,Text,View, Platform,SafeAreaView ,ScrollView,StyleSheet , Image, AsyncStorage} from 'react-native';
 
 import SpotList from '../componets/spotList';
 
 import logo from '../assets/logo.png';
 
-export default function List(){
+    export default function List({navigation}){
 
     const [techs, setTechs]  =useState([]);
 
@@ -24,6 +24,12 @@ export default function List(){
 
     }, []) */
 
+    function handdleLogout(){
+        AsyncStorage.removeItem('user');
+
+        navigation.navigate('Login');
+    }   
+
     useEffect(() => {
         AsyncStorage.getItem('techs')
             .then((storagedTechs) => {
@@ -35,7 +41,13 @@ export default function List(){
 
     return (
     <SafeAreaView style = {styles.container}>
-        <Image style = {styles.logo} source = {logo}/>
+        <View style={{flex: 0.2, flexDirection: 'row-reverse'}}>
+            <TouchableOpacity  onPress={handdleLogout} style = {styles.button}>
+                <Text style = {styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+            <Image style = {styles.logo} source = {logo}/>
+        </View>
+        
         <ScrollView>
                 { techs.map(tech=> <SpotList key={tech} tech={tech} /> )}
             </ScrollView>
@@ -55,6 +67,23 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
         alignSelf: 'center',
         marginTop: 10,
+        
+    },
+
+    button : {
+        marginTop: 30,
+        height:30,
+        width: 100,
+        backgroundColor : '#f05a5b',
+        justifyContent:'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+
+    buttonText : {
+        color: '#fff',
+        fontWeight: 'bold',  
+        fontSize: 15,
         
     }
 
